6f0995819c5faa5f3ef995892d842a611013fd75
Nycollas Henich <nhhenich@gmail.com>
2024-07-02 14:22:24 -0300

    forms

--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
 from typing import Any
-from django.views.generic import TemplateView
+from django.views.generic import FormView
 from .models import Servico, Funcionario, Recurso, Preco
+from .forms import ContatoForm
+from django.urls import reverse_lazy
+from django.contrib import messages
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs: Any):
         context = super(IndexView, self).get_context_data(**kwargs)
@@ -14,3 +19,11 @@ class IndexView(TemplateView):
         context ['precos'] = Preco.objects.all()
         return context
 
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+    
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar o E-mail')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
--- setup/settings.py
+++ setup/settings.py
@@ -130,3 +130,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Test console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Email Produção
+'''
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = 'no-reply@fusion.com.br'
+EMAIL_PORT = 587
+EMAIL_USE_TSL = True
+EMAIL_HOST_PASSWORD = 'fusion'
+DEFAULT_FROM_EMAIL = 'contato@fusion.com.br'
+'''
